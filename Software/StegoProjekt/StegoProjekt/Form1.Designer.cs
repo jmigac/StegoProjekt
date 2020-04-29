9a87de7b6714edec8a9860cef1557a305f4595b9
Jurica Migač <jurica1.migac@gmail.com>
2020-04-30 01:16:01 +0200

    06 Obavljeno pozivanje funkcija iz potrebnih klasa.

--- Software/StegoProjekt/StegoProjekt/Form1.Designer.cs
+++ Software/StegoProjekt/StegoProjekt/Form1.Designer.cs
@@ -33,7 +33,14 @@
             this.uiInputTekstZaUgradnju = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.uiActionUgradiTekst = new System.Windows.Forms.Button();
+            this.uiOutputSlikaZaIzvoz = new System.Windows.Forms.PictureBox();
+            this.uiActionUcitajSlikuZaIzvoz = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uiOutputTekstZaIzvoz = new System.Windows.Forms.TextBox();
+            this.uiActionSpremiSliku = new System.Windows.Forms.Button();
+            this.uiActionSpremiSadrzajSteganografije = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlikaZaUcitavanje)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlikaZaIzvoz)).BeginInit();
             this.SuspendLayout();
             // 
             // uiOutputSlikaZaUcitavanje
@@ -81,12 +88,75 @@
             this.uiActionUgradiTekst.TabIndex = 4;
             this.uiActionUgradiTekst.Text = "Ugradi tekst";
             this.uiActionUgradiTekst.UseVisualStyleBackColor = true;
+            this.uiActionUgradiTekst.Click += new System.EventHandler(this.uiActionUgradiTekst_Click);
+            // 
+            // uiOutputSlikaZaIzvoz
+            // 
+            this.uiOutputSlikaZaIzvoz.Location = new System.Drawing.Point(184, 13);
+            this.uiOutputSlikaZaIzvoz.Name = "uiOutputSlikaZaIzvoz";
+            this.uiOutputSlikaZaIzvoz.Size = new System.Drawing.Size(165, 133);
+            this.uiOutputSlikaZaIzvoz.TabIndex = 5;
+            this.uiOutputSlikaZaIzvoz.TabStop = false;
+            // 
+            // uiActionUcitajSlikuZaIzvoz
+            // 
+            this.uiActionUcitajSlikuZaIzvoz.Location = new System.Drawing.Point(184, 153);
+            this.uiActionUcitajSlikuZaIzvoz.Name = "uiActionUcitajSlikuZaIzvoz";
+            this.uiActionUcitajSlikuZaIzvoz.Size = new System.Drawing.Size(165, 23);
+            this.uiActionUcitajSlikuZaIzvoz.TabIndex = 6;
+            this.uiActionUcitajSlikuZaIzvoz.Text = "Učitaj sliku";
+            this.uiActionUcitajSlikuZaIzvoz.UseVisualStyleBackColor = true;
+            this.uiActionUcitajSlikuZaIzvoz.Click += new System.EventHandler(this.uiActionUcitajSlikuZaIzvoz_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tekst iz steganografske slike:";
+            // 
+            // uiOutputTekstZaIzvoz
+            // 
+            this.uiOutputTekstZaIzvoz.Enabled = false;
+            this.uiOutputTekstZaIzvoz.Location = new System.Drawing.Point(184, 197);
+            this.uiOutputTekstZaIzvoz.Multiline = true;
+            this.uiOutputTekstZaIzvoz.Name = "uiOutputTekstZaIzvoz";
+            this.uiOutputTekstZaIzvoz.Size = new System.Drawing.Size(165, 129);
+            this.uiOutputTekstZaIzvoz.TabIndex = 8;
+            // 
+            // uiActionSpremiSliku
+            // 
+            this.uiActionSpremiSliku.Location = new System.Drawing.Point(13, 358);
+            this.uiActionSpremiSliku.Name = "uiActionSpremiSliku";
+            this.uiActionSpremiSliku.Size = new System.Drawing.Size(165, 23);
+            this.uiActionSpremiSliku.TabIndex = 9;
+            this.uiActionSpremiSliku.Text = "Spremi sliku";
+            this.uiActionSpremiSliku.UseVisualStyleBackColor = true;
+            this.uiActionSpremiSliku.Click += new System.EventHandler(this.uiActionSpremiSliku_Click);
+            // 
+            // uiActionSpremiSadrzajSteganografije
+            // 
+            this.uiActionSpremiSadrzajSteganografije.Location = new System.Drawing.Point(185, 332);
+            this.uiActionSpremiSadrzajSteganografije.Name = "uiActionSpremiSadrzajSteganografije";
+            this.uiActionSpremiSadrzajSteganografije.Size = new System.Drawing.Size(164, 23);
+            this.uiActionSpremiSadrzajSteganografije.TabIndex = 10;
+            this.uiActionSpremiSadrzajSteganografije.Text = "Spremi u .txt";
+            this.uiActionSpremiSadrzajSteganografije.UseVisualStyleBackColor = true;
+            this.uiActionSpremiSadrzajSteganografije.Click += new System.EventHandler(this.uiActionSpremiSadrzajSteganografije_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(188, 358);
+            this.ClientSize = new System.Drawing.Size(361, 388);
+            this.Controls.Add(this.uiActionSpremiSadrzajSteganografije);
+            this.Controls.Add(this.uiActionSpremiSliku);
+            this.Controls.Add(this.uiOutputTekstZaIzvoz);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.uiActionUcitajSlikuZaIzvoz);
+            this.Controls.Add(this.uiOutputSlikaZaIzvoz);
             this.Controls.Add(this.uiActionUgradiTekst);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.uiInputTekstZaUgradnju);
@@ -95,6 +165,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlikaZaUcitavanje)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlikaZaIzvoz)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +178,12 @@
         private System.Windows.Forms.TextBox uiInputTekstZaUgradnju;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button uiActionUgradiTekst;
+        private System.Windows.Forms.PictureBox uiOutputSlikaZaIzvoz;
+        private System.Windows.Forms.Button uiActionUcitajSlikuZaIzvoz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uiOutputTekstZaIzvoz;
+        private System.Windows.Forms.Button uiActionSpremiSliku;
+        private System.Windows.Forms.Button uiActionSpremiSadrzajSteganografije;
     }
 }
 
--- Software/StegoProjekt/StegoProjekt/Form1.cs
+++ Software/StegoProjekt/StegoProjekt/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace StegoProjekt
 {
     public partial class Form1 : Form
     {
+        private Bitmap slikaZaUvozSteganografije = null;
+        private Bitmap slikaZaSkrivenimTekstom = null;
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +27,133 @@ namespace StegoProjekt
 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                //uiOutputSlikaZaUcitavanje.ImageLocation = ofd.FileName;
-                Bitmap slika = new Bitmap(ofd.FileName);
-                uiOutputSlikaZaUcitavanje.Image = Image.FromFile(ofd.FileName);
-                uiInputTekstZaUgradnju.Enabled = true;
-                uiActionUgradiTekst.Enabled = true;
-                uiActionUcitajSliku.Enabled = false;
+                slikaZaUvozSteganografije = PostaviElementSlike(ofd);
+            }
+
+        }
+        private Bitmap PostaviElementSlike(OpenFileDialog ofd)
+        {
+            Bitmap slika = new Bitmap(ofd.FileName);
+            uiOutputSlikaZaUcitavanje.Image = Image.FromFile(ofd.FileName);
+            uiInputTekstZaUgradnju.Enabled = true;
+            uiActionUgradiTekst.Enabled = true;
+            uiActionUcitajSliku.Enabled = false;
+            return slika;
+        }
+
+        private void uiActionUgradiTekst_Click(object sender, EventArgs e)
+        {
+            StegoImporter importer = new StegoImporter(slikaZaUvozSteganografije, uiInputTekstZaUgradnju.Text);
+            try
+            {
+                slikaZaSkrivenimTekstom = importer.StegoSlika;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            uiActionUgradiTekst.Enabled = false;
+            uiActionSpremiSliku.Focus();
+        }
+
+        private void SpremiSliku(Bitmap slikaZaSpremanje)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = OgranicenjaDialoga.SaveDialogFilter;
+            sfd.Title = OgranicenjaDialoga.SaveDialogTitle;
+            sfd.DefaultExt = "jpg";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                if (ProvjeriNazivSpremljeneDatoteke(sfd.FileName))
+                {
+                    switch (sfd.FilterIndex)
+                    {
+                        case 1:
+                            slikaZaSpremanje.Save(sfd.FileName);
+                            break;
+                        case 2:
+                            slikaZaSpremanje.Save(sfd.FileName);
+                            break;
+                        case 3:
+                            System.IO.FileStream fs = sfd.OpenFile() as System.IO.FileStream;
+                            slikaZaSpremanje.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                            fs.Close();
+                            break;
+                    }
+                }
+            }
+        }
+
+        private bool ProvjeriNazivSpremljeneDatoteke(string nazivDatoteke)
+        {
+            if (nazivDatoteke != string.Empty)
+                return true;
+            else return false;
+        }
+
+        private void uiActionUcitajSlikuZaIzvoz_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            Bitmap slikaZaSteganografiju = null;
+            ofd.Filter = "All picture files| *.bmp; *.dib; *.jpg; *.jpeg; *.jpe; *.jfif; *.tif; *.tiff; *.gif; *.png | Bitmap files(*.bmp, *.dib) | *.bmp; *.dib | JPEG(*.jpg, *.jpeg, *.jpe, *.jfif) | *.jpg; *.jpeg; *.jpe; *.jfif | TIFF(*.tif, *.tiff) | *.tif; *.tiff | GIF(*.gif) | *.gif | PNG(*.png) | *.png";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                slikaZaSteganografiju = PostaviElementSlikeZaIzvoz(ofd);
+            }
+            StegoExporter exporter = new StegoExporter(slikaZaSteganografiju);
+            try
+            {
+                exporter.Export();
+                uiOutputTekstZaIzvoz.Text = exporter.Tekst;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private Bitmap PostaviElementSlikeZaIzvoz(OpenFileDialog ofd)
+        {
+            Bitmap slika = new Bitmap(ofd.FileName);
+            uiOutputSlikaZaIzvoz.Image = Image.FromFile(ofd.FileName);
+            return slika;
+        }
+
+        private void uiActionSpremiSliku_Click(object sender, EventArgs e)
+        {
+            SpremiSliku(slikaZaSkrivenimTekstom);
+            uiActionSpremiSliku.Enabled = false;
+            uiActionUcitajSliku.Enabled = true;
+            uiOutputSlikaZaUcitavanje.Image = null;
+            uiInputTekstZaUgradnju.Clear();
+        }
+
+        private void uiActionSpremiSadrzajSteganografije_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = OgranicenjaDialoga.SaveDialogTxt;
+            sfd.Title = OgranicenjaDialoga.SaveDialogTitle;
+            sfd.DefaultExt = "txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                if (ProvjeriNazivSpremljeneDatoteke(sfd.FileName))
+                {
+                    switch (sfd.FilterIndex)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                            FileStream fParameter = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+                            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+                            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+                            m_WriterParameter.Write(uiOutputTekstZaIzvoz.Text);
+                            m_WriterParameter.Flush();
+                            m_WriterParameter.Close();
+                            fParameter.Close();
+                            break;
+                    }
+                }
             }
         }
     }
